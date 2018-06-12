b15c476cb83c860dc699ae8c4ccbd4bb7a7b8efe
AleksandraKaminska <26117538+AleksandraKaminska@users.noreply.github.com>
2018-06-12 18:01:29 +0200

    added first component and styles prepared first request for movie api

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+https://developers.themoviedb.org/3/getting-started/introduction
+
+https://www.googleapis.com/books
+
+https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+
+
+MOVIE SEARCH: https://api.themoviedb.org/3/search/multi?api_key=92b2df3080b91d92b31eacb015fc5497&query=Harry+Potter+and+the+Philosopher%27s+Stone
+
+BOOK SEARCH: https://www.googleapis.com/books/v1/volumes?q=Harry+Potter+and+the+Philosopher%27s+Stone
+
+ITUNES SEARCH API:
+```javascript
+fetch('https://itunes.apple.com/search?term=Harry+Potter&entity=movie,ebook')
+.then(resp => resp.json())
+.then(data => console.log(data.results))
+```
--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: [],
+      books: []
+    }
+  }
+
+  componentDidMount() {
+    /*const baseUrl = 'https://api.themoviedb.org/3/search/multi?api_key=92b2df3080b91d92b31eacb015fc5497&query='
+    fetch(`${baseUrl}Harry+Potter`)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        movies: data.results
+      })
+    })*/
+
+    fetch('https://itunes.apple.com/search?term=Harry+Potter&entity=movie,ebook')
+    .then(resp => resp.json())
+    .then(({ results }) => {
+      this.setState({
+        movies: results.filter(e => e.kind.indexOf('movie') > -1),
+        books: results.filter(e => e.kind.indexOf('book') > -1)
+      })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <a href="/">
+            <h1>Bookie</h1>
+          </a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <input
+            aria-hidden="true"
+            type="text"
+            placeholder="search"
+          />
+          <div className="results">
+            <h2>Movies</h2>
+            {
+              this.state.movies.map(e => (
+                <a href={`/movie_id/${e.trackId}`} key={e.trackId}>
+                  <li>{e.trackName}</li>
+                </a>
+              ))
+            }
+            <h2>Books</h2>
+            {
+              this.state.books.map((e, i) => (
+                <a href={`/book_id/${e.trackId}`} key={e.trackId}>
+                  <li>{e.trackName}</li>
+                </a>
+              ))
+            }
+          </div>
+        </main>
       </div>
     );
   }
